9cbeebe3fc6d46a064d9409d6937599a79932614
elias2389 <andreselias2303@gmail.com>
2020-12-05 18:24:28 -0300

    Refactor code

--- app/src/main/java/com/ae/qrapplication/MainActivity.kt
+++ app/src/main/java/com/ae/qrapplication/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.ae.qrapplication
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.MultiFormatWriter
+import com.google.zxing.common.BitMatrix
 import com.journeyapps.barcodescanner.BarcodeEncoder
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var createQrButton: Button
     private lateinit var imageQrView: ImageView
+
+    private val QR_IMAGE_SIZE: Int = 500
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,22 +32,32 @@ class MainActivity : AppCompatActivity() {
 
     private fun fetchQrCode() {
         createQrButton.setOnClickListener {
-            generateQrCode()
+            setupBitmapQ()
         }
     }
 
-
-    private fun generateQrCode() {
+    private fun setupBitmapQ() {
       try {
-          val multiFormatWriter = MultiFormatWriter()
-          val bitMatrix = multiFormatWriter.encode("Andres test", BarcodeFormat.QR_CODE, 500, 500)
-          val barcodeEncoder = BarcodeEncoder()
-          val bitmap = barcodeEncoder.createBitmap(bitMatrix)
-
-          imageQrView.setImageBitmap(bitmap)
+          getBitMatrix().let {
+              val bitmap = createBitmap(it)
+              imageQrView.setImageBitmap(bitmap)
+          }
       } catch (e: Exception) {
           e.printStackTrace()
       }
     }
 
+    private fun getBitMatrix(): BitMatrix {
+        return MultiFormatWriter()
+            .encode(
+                "Test to generate bar code",
+                BarcodeFormat.QR_CODE,
+                QR_IMAGE_SIZE,
+                QR_IMAGE_SIZE
+            )
+    }
+
+    private fun createBitmap(bitMatrix: BitMatrix): Bitmap {
+        return BarcodeEncoder().createBitmap(bitMatrix)
+    }
 }
